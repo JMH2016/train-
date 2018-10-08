8681223631112d933092e77c13a7f9294e8e608f
JamieHo2018 <jamie-ho2009@hotmail.com>
2018-10-08 13:48:54 -0700

    20181008-1

---
+++ assets/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyAHXcYzxEdE_v3XokOYyiQ6efISkO8dMhA",
+    authDomain: "dude-storage-tsm.firebaseapp.com",
+    databaseURL: "https://dude-storage-tsm.firebaseio.com",
+    projectId: "dude-storage-tsm",
+    storageBucket: "dude-storage-tsm.appspot.com",
+    messagingSenderId: "680489550103"
+  };
